ce288194f82454cefb01bbb37d4f6ba5cfc615fe
Lizeth Pena <lilith_317@hotmail.com>
2018-08-16 11:21:50 -0500

    render json

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-  		render template: "home/index"
+  		render json:{status: 'SUCCESS', message: 'Loaded articles successfully', data: articles}, 
+    status: :ok
   end
 end
